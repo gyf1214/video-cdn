d8357961fe1d0329b548f7f02f1732a3014919bb
gyf1214 <gyf1214@gmail.com>
2017-12-19 02:08:26 +0800

    .

--- src/nameserver/main.c
+++ src/nameserver/main.c
@@ -1,13 +1,16 @@
 #include "common.h"
 #include "config.h"
 #include "server.h"
+#include "util.h"
 
 int main(int argc, char **argv) {
     config.parse(argc, argv);
 
+    util.init();
     server.create(&config.local);
-    server.loop();
-    fclose(config.logging);
 
+    server.loop();
+    
+    fclose(config.logging);
     return 0;
 }
